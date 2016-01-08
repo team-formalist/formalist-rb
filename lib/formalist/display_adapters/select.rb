579db62e66be5c5968aa67b22b05dfa8e8b5e189
Tim Riley <tim@openmonkey.com>
2016-01-08 21:04:19 +1100

    Tweak wording

--- lib/formalist/display_adapters/select.rb
+++ lib/formalist/display_adapters/select.rb
@@ -3,7 +3,7 @@ module Formalist
     class Select
       def call(field)
         raise ArgumentError, "field type must be string or int" unless %w[string int].include?(field.type)
-        raise ArgumentError, "field must supply +option_values+ config" unless field.config.keys.include?(:option_values)
+        raise ArgumentError, "field must have +option_values+ config" unless field.config.keys.include?(:option_values)
 
         field.with(display_variant: "select")
       end
