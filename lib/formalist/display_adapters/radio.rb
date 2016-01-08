3ce515a13b5df25207940580877129e83ef74fdd
Tim Riley <tim@openmonkey.com>
2016-01-08 21:29:47 +1100

    Flesh out “select” and “radio” display adapters

--- lib/formalist/display_adapters/radio.rb
+++ lib/formalist/display_adapters/radio.rb
@@ -1,18 +1,18 @@
 module Formalist
   class DisplayAdapters
-    class Select
+    class Radio
       PERMITTED_TYPES = %w[
         decimal
         float
         int
         string
-      ].freeze
+      ]
 
       def call(field)
         raise ArgumentError, "field type must be one of #{PERMITTED_TYPES.join(', ')}" unless PERMITTED_TYPES.include?(field.type)
         raise ArgumentError, "field must have +option_values+ config" unless field.config.keys.include?(:option_values)
 
-        field.to_display_variant("select")
+        field.to_display_variant("radio")
       end
     end
   end
