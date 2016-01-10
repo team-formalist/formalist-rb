c6cafda2eced1071a7e67d89d85561f6194d0ea9
Tim Riley <tim@openmonkey.com>
2016-01-11 08:56:41 +1100

    Add a spec demonstrating a custom display adapters container

--- spec/integration/display_adapters_spec.rb
+++ spec/integration/display_adapters_spec.rb
@@ -19,4 +19,30 @@ RSpec.describe "Display adapters" do
       ]]
     ]
   end
+
+  it "supports custom disply adapters in a provided container" do
+    adapter_class = Class.new do
+      def call(field)
+        field.to_display_variant("custom")
+      end
+    end
+
+    container = Class.new(Formalist::DisplayAdapters) do
+      register "custom", adapter_class.new
+    end
+
+    form = Class.new(Formalist::Form) do
+      configure do |config|
+        config.display_adapters = container
+      end
+
+      field :name, type: "string", display: "custom"
+      field :email, type: "string"
+    end.new
+
+    expect(form.({}).to_ary).to eq [
+      [:field, [:name, "string", "custom", nil, [], []]],
+      [:field, [:email, "string", "default", nil, [], []]],
+    ]
+  end
 end
