4f506893486b27f72e4f2636c7b5f9d15d01530c
Tim Riley <tim@openmonkey.com>
2016-01-18 11:54:04 +1100

    Support value-local predicate lists on attr elements

--- lib/formalist/form/result/attr.rb
+++ lib/formalist/form/result/attr.rb
@@ -1,19 +1,24 @@
+require "formalist/validation/collection_rules_compiler"
 require "formalist/validation/value_rules_compiler"
-
+require "formalist/validation/predicate_list_compiler"
 
 module Formalist
   class Form
     class Result
       class Attr
-        attr_reader :definition, :input, :rules, :errors
+        attr_reader :definition, :input, :value_rules, :value_predicates, :collection_rules, :errors
         attr_reader :children
 
         def initialize(definition, input, rules, errors)
-          rules_compiler = Validation::ValueRulesCompiler.new(definition.name)
+          value_rules_compiler = Validation::ValueRulesCompiler.new(definition.name)
+          value_predicates_compiler = Validation::PredicateListCompiler.new
+          collection_rules_compiler = Validation::CollectionRulesCompiler.new(definition.name)
 
           @definition = definition
           @input = input.fetch(definition.name, {})
-          @rules = rules_compiler.(rules)
+          @value_rules = value_rules_compiler.(rules)
+          @value_predicates = value_predicates_compiler.(@value_rules)
+          @collection_rules = collection_rules_compiler.(rules)
           @errors = errors.fetch(definition.name, [])[0] || []
           @children = build_children
         end
@@ -25,12 +30,11 @@ module Formalist
           # Errors, if the attr hash hasn't been provided
           # {:meta=>[["meta is missing"], nil]}
 
-          local_rules = rules # TODO
           local_errors = errors[0].is_a?(Hash) ? [] : errors
 
           [:attr, [
             definition.name,
-            local_rules,
+            value_predicates,
             local_errors,
             children.map(&:to_ary),
           ]]
@@ -40,7 +44,7 @@ module Formalist
 
         def build_children
           child_errors = errors[0].is_a?(Hash) ? errors[0] : {}
-          definition.children.map { |el| el.(input, rules, child_errors) }
+          definition.children.map { |el| el.(input, collection_rules, child_errors) }
         end
       end
     end
