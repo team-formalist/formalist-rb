644f2ce13ca21f08f28c48ac68962cc49a018ac1
Max Wheeler <max@makenosound.com>
2016-03-10 09:18:41 +1100

    Add `code` attributes to text-type fields.

--- lib/formalist/elements/standard/text_area.rb
+++ lib/formalist/elements/standard/text_area.rb
@@ -7,6 +7,7 @@ module Formalist
     class TextArea < Field
       attribute :text_size, Types::String.enum("xsmall", "small", "normal", "large", "xlarge"), default: "normal"
       attribute :box_size, Types::String.enum("single", "small", "normal", "large", "xlarge"), default: "normal"
+      attribute :code Types::Bool
     end
 
     register :text_area, TextArea
