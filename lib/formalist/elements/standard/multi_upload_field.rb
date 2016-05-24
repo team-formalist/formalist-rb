5def8e5cc376b4ddb6b76e06e9f94d7ff5c03a53
Chris Buttery <info@chrisbuttery.com>
2016-05-24 11:11:11 +1000

    Adds upload_label & button_label attributes.
    Adds above attributes to the multi_upload_field
    and upload_field elements

--- lib/formalist/elements/standard/multi_upload_field.rb
+++ lib/formalist/elements/standard/multi_upload_field.rb
@@ -7,6 +7,8 @@ module Formalist
     class MultiUploadField < Field
       attribute :presign_url, Types::String
       attribute :render_uploaded_as, Types::String
+      attribute :upload_label, Types::String
+      attribute :button_label, Types::String
     end
 
     register :multi_upload_field, MultiUploadField
--- lib/formalist/elements/standard/upload_field.rb
+++ lib/formalist/elements/standard/upload_field.rb
@@ -7,6 +7,8 @@ module Formalist
     class UploadField < Field
       attribute :presign_url, Types::String
       attribute :render_uploaded_as, Types::String
+      attribute :upload_label, Types::String
+      attribute :button_label, Types::String
     end
 
     register :upload_field, UploadField
