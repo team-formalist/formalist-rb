30cfbdb4c955f6844f79c05b59db3b5691e1a98c
Narinda Reeders <narindajane@gmail.com>
2016-06-01 10:22:23 +1000

    Early stages of an automated test of draft js compiler.

---
+++ spec/integration/draft_js_compiler_spec.rb
@@ -0,0 +1,18 @@
+require "formalist/draft_js_compiler"
+
+RSpec.describe Formalist::DraftJSCompiler do
+  subject(:compiler) { Formalist::DraftJSCompiler.new }
+
+  let(:ast) {
+    block1 = ["block", ["header-two", "a34sd", [["inline", [[], "Heading"] ] ] ]]
+    block2 = ["block", ["unordered-list-item", "dodnk", [["inline", [[], "I am more "] ], ["entity", ["LINK", "3", "MUTABLE", {url: "http://makenosound.com"}, [["inline", [[], "content"] ] ] ] ], ["inline", [[], "."] ] ] ] ]
+
+    [block1, block2, block2, block1]
+  }
+
+  it "works" do
+    expect(compiler.call(ast)).to_not be_nil
+  end
+
+
+end
