c29f82bde860b69b7425495e3bc60f7c9184164d
Max Wheeler <max@makenosound.com>
2016-08-01 15:41:09 +1000

    Allow additional attribute for the rte field.

--- lib/formalist/elements/standard/rich_text_area.rb
+++ lib/formalist/elements/standard/rich_text_area.rb
@@ -7,6 +7,8 @@ module Formalist
     class RichTextArea < Field
       attribute :box_size, Types::String.enum("single", "small", "normal", "large", "xlarge"), default: "normal"
       attribute :inline_formatters, Types::Array
+      attribute :block_formatters, Types::Array
+      attribute :embeddable_forms, Types::Hash
     end
 
     register :rich_text_area, RichTextArea
