67e33e29207511a1ab0bd725b755f3775c3c5ce2
Max Wheeler <max@makenosound.com>
2016-08-05 14:11:29 +1000

    Add search_selection_field (with multi variant).

---
+++ lib/formalist/elements/standard/search_multi_selection_field.rb
@@ -0,0 +1,20 @@
+require "formalist/element"
+require "formalist/elements"
+require "formalist/types"
+
+module Formalist
+  class Elements
+    class SearchMultiSelectionField < Field
+      attribute :selector_label, Types::String
+      attribute :render_option_as, Types::String
+      attribute :render_selection_as, Types::String
+      attribute :search_url, Types::String
+      attribute :search_per_page, Types::Int
+      attribute :search_params, Types::Hash
+      attribute :search_threshold, Types::Int
+      attribute :selections, Types::Array
+    end
+
+    register :search_multi_selection_field, SearchMultiSelectionField
+  end
+end
