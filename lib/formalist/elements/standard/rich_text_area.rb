62d106abb50345d7d946ea15763eac36a6163b8e
Tim Riley <tim@openmonkey.com>
2016-08-18 14:56:28 +1000

    Convert embeddable forms to blank ASTs when generating RTE AST

--- lib/formalist/elements/standard/rich_text_area.rb
+++ lib/formalist/elements/standard/rich_text_area.rb
@@ -8,7 +8,22 @@ module Formalist
       attribute :box_size, Types::String.enum("single", "small", "normal", "large", "xlarge"), default: "normal"
       attribute :inline_formatters, Types::Array
       attribute :block_formatters, Types::Array
-      attribute :embeddable_forms, Types::Hash
+      attribute :embeddable_forms, Types::Strict::Hash # TODO: need better typing, perhaps even require a rich object rather than a hash
+
+      # FIXME: it would be tidier to have a reader method for each attribute
+      def attributes
+        attrs = super
+
+        # Replace the form objects with their AST
+        attrs.merge(
+          embeddable_forms: attrs[:embeddable_forms].map { |key, attrs|
+            original_attrs = attrs
+            adjusted_attrs = original_attrs.merge(template: original_attrs[:template].build.to_ast)
+
+            [key, adjusted_attrs]
+          }.to_h
+        )
+      end
     end
 
     register :rich_text_area, RichTextArea
