77804a78044aef626d7cd1f4c03bcb74e099fe0b
Tim Riley <tim@openmonkey.com>
2016-08-30 15:14:58 +1000

    Run rich text area input text through the embedded form compiler
    This allows the rich text string to have its embedded forms fully “hydrated” for use by the draft.js field on the front-end without any extra user effort.

--- lib/formalist/elements/standard/rich_text_area.rb
+++ lib/formalist/elements/standard/rich_text_area.rb
@@ -1,6 +1,7 @@
 require "formalist/element"
 require "formalist/elements"
 require "formalist/types"
+require "formalist/rich_text/embedded_form_compiler"
 
 module Formalist
   class Elements
@@ -8,21 +9,31 @@ module Formalist
       attribute :box_size, Types::String.enum("single", "small", "normal", "large", "xlarge"), default: "normal"
       attribute :inline_formatters, Types::Array
       attribute :block_formatters, Types::Array
-      attribute :embeddable_forms, Types::Strict::Hash # TODO: need better typing, perhaps even require a rich object rather than a hash
+      attribute :embeddable_forms, Types::Dependency.constrained(respond_to: :to_h)
 
       # FIXME: it would be tidier to have a reader method for each attribute
       def attributes
-        attrs = super
-
         # Replace the form objects with their AST
-        attrs.merge(
-          embeddable_forms: Hash(attrs[:embeddable_forms]).map { |key, attrs|
-            original_attrs = attrs
-            adjusted_attrs = original_attrs.merge(form: original_attrs[:form].build.to_ast)
+        super.merge(embeddable_forms: embeddable_forms_config)
+      end
 
-            [key, adjusted_attrs]
-          }.to_h
-        )
+      def embeddable_forms_config
+        Hash(@attributes[:embeddable_forms].to_h).map { |key, attrs|
+          original_attrs = attrs
+          adjusted_attrs = original_attrs.merge(form: original_attrs[:form].build.to_ast)
+
+          [key, adjusted_attrs]
+        }.to_h
+      end
+
+      def input
+        input_compiler.(@input)
+      end
+
+      private
+
+      def input_compiler
+        RichText::EmbeddedFormCompiler.new(@attributes[:embeddable_forms])
       end
     end
 
