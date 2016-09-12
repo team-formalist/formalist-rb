982677be92a6c7e4f582aa986eacf5ac5135c7cb
Tim Riley <tim@openmonkey.com>
2016-09-12 11:27:54 +1000

    Move rich text compiler spec across to unit specs

---
+++ spec/unit/rich_text/html_compiler_spec.rb
@@ -0,0 +1,29 @@
+require "formalist/rich_text/html_compiler"
+
+RSpec.describe Formalist::RichText::HTMLCompiler do
+  let(:ast) {
+    block1 = ["block", ["header-two", "a34sd", [["inline", [[], "Heading"]]]]]
+    block2 = ["block", ["unordered-list-item", "dodnk", [["inline", [[], "I am more "]], ["entity", ["LINK", "3", "MUTABLE", {url: "http://makenosound.com"}, [["inline", [[], "content"]]]]], ["inline", [[], "."]]]]]
+
+    [block1, block2, block2, block1]
+  }
+
+  describe "with default HTML renderer" do
+    let(:compiler) { described_class.new }
+
+    describe "#call" do
+      subject(:html) { compiler.(ast) }
+
+      it "compiles to HTML" do
+        is_expected.to eq <<-HTML.gsub(/^\s+/, "").gsub("\n", "")
+          <h2 data-key='a34sd'>Heading</h2>
+          <ul>
+            <li data-key='dodnk'>I am more <a data-entity-key='3' href='http://makenosound.com'>content</a>.</li>
+            <li data-key='dodnk'>I am more <a data-entity-key='3' href='http://makenosound.com'>content</a>.</li>
+          </ul>
+          <h2 data-key='a34sd'>Heading</h2>
+        HTML
+      end
+    end
+  end
+end
