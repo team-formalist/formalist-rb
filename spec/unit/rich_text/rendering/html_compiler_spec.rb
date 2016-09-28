83c114b978b00fd655e58836ad319a9ae838990c
Tim Riley <tim@openmonkey.com>
2016-09-28 16:22:43 +1000

    Update HTML compiler spec

--- spec/unit/rich_text/rendering/html_compiler_spec.rb
+++ spec/unit/rich_text/rendering/html_compiler_spec.rb
@@ -1,6 +1,8 @@
-require "formalist/rich_text/html_compiler"
+require "formalist/rich_text/rendering/html_compiler"
+require "formalist/rich_text/rendering/html_renderer"
+require "formalist/rich_text/rendering/embedded_form_renderer"
 
-RSpec.describe Formalist::RichText::HTMLCompiler do
+RSpec.describe Formalist::RichText::Rendering::HTMLCompiler do
   let(:ast) {
     block1 = ["block", ["header-two", "a34sd", [["inline", [[], "Heading"]]]]]
     block2 = ["block", ["unordered-list-item", "dodnk", [["inline", [[], "I am more "]], ["entity", ["LINK", "3", "MUTABLE", {url: "http://makenosound.com"}, [["inline", [[], "content"]]]]], ["inline", [[], "."]]]]]
@@ -9,7 +11,12 @@ RSpec.describe Formalist::RichText::HTMLCompiler do
   }
 
   describe "with default HTML renderer" do
-    let(:compiler) { described_class.new }
+    let(:compiler) {
+      described_class.new(
+        html_renderer: Formalist::RichText::Rendering::HTMLRenderer.new,
+        embedded_form_renderer: Formalist::RichText::Rendering::EmbeddedFormRenderer.new,
+      )
+    }
 
     describe "#call" do
       subject(:html) { compiler.(ast) }
