270bfaf99cfad2f8141c2744648815811264a84b
Tim Riley <tim@openmonkey.com>
2016-10-21 22:11:44 +1100

    Accept extra options to be used when rendering embedded forms

--- lib/formalist/rich_text/rendering/embedded_form_renderer.rb
+++ lib/formalist/rich_text/rendering/embedded_form_renderer.rb
@@ -3,14 +3,18 @@ module Formalist
     module Rendering
       class EmbeddedFormRenderer
         attr_reader :container
+        attr_reader :render_options
 
-        def initialize(container = {})
+        def initialize(container: {}, render_options: {})
           @container = container
+          @render_options = render_options
         end
 
         def call(form_data)
-          if container.key?(form_data["name"])
-            container[form_data["name"]].(form_data["data"])
+          type, data = form_data.values_at("name", "data")
+
+          if container.key?(type)
+            container[type].(data, render_options)
           else
             ""
           end
