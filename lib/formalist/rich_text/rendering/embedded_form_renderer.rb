ddde0f5edcbba4ee11953e080a431a996d066335
Andrew Croome <andrewcroome@gmail.com>
2016-11-28 11:49:42 +1100

    Add another temporary fix for rendering embedded form data as either strings or keys.

--- lib/formalist/rich_text/rendering/embedded_form_renderer.rb
+++ lib/formalist/rich_text/rendering/embedded_form_renderer.rb
@@ -13,6 +13,11 @@ module Formalist
         def call(form_data)
           type, data = form_data.values_at("name", "data")
 
+          # FIXME: temporary fix for handling form_data keys
+          # as either strings or symbols.
+          type = form_data[:name] unless type
+          data = form_data[:data] unless data
+
           if container.key?(type)
             container[type].(data, render_options)
           else
