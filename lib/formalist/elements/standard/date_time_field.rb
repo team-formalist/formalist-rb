cdea4ec02a6c013d90f1a852f2dffdb8bca733a4
Andrew Croome <andrewcroome@gmail.com>
2018-07-17 17:12:13 +1000

    Add support for custom time formats on date time fields.

--- lib/formalist/elements/standard/date_time_field.rb
+++ lib/formalist/elements/standard/date_time_field.rb
@@ -5,6 +5,8 @@ require "formalist/types"
 module Formalist
   class Elements
     class DateTimeField < Field
+      attribute :time_format, Types::String
+      attribute :human_time_format, Types::String
     end
 
     register :date_time_field, DateTimeField
