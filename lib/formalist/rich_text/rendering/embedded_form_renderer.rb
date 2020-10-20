1a701e8d7a8dae6ef1ab5692de849e8eeddf0135
narinda <narindajane@gmail.com>
2020-10-20 12:58:29 +1100

    Add namespace and paths options to embedded form renderer class
    This simplifies setup by allowing namespace filtering as an option to the renderer.
    
    It also adds a paths option to enable subfolders to be setup. These can be used to setup view overrides in different contexts.

--- lib/formalist/rich_text/rendering/embedded_form_renderer.rb
+++ lib/formalist/rich_text/rendering/embedded_form_renderer.rb
@@ -3,21 +3,39 @@ module Formalist
     module Rendering
       class EmbeddedFormRenderer
         attr_reader :container
+        attr_reader :namespace
+        attr_reader :paths
         attr_reader :options
 
-        def initialize(container = {}, **options)
+        def initialize(container = {}, namespace: nil, paths: [], **options)
           @container = container
+          @namespace = namespace
+          @paths = paths
           @options = options
         end
 
         def call(form_data)
           type, data = form_data.values_at(:name, :data)
 
-          if container.key?(type)
-            container[type].(data, options)
+          key = resolve_key(type)
+
+          if key
+            container[key].(data, options)
           else
             ""
           end
+        end
+
+        private
+
+        def resolve_key(type)
+          paths.each do |path|
+            path_key = path.tr("/", ".")
+            key = [namespace, path_key, type].compact.join(".")
+            return key if container.key?(key)
+          end
+          key =  [namespace, type].compact.join(".")
+          return key if container.key?(key)
         end
       end
     end
