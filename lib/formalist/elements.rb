21ca69edbe7baea4cf88190874ee564de9e7f6d3
narinda <narindajane@gmail.com>
2020-10-29 18:02:01 +1100

    Add many forms support with child data as a hash resembling input data

--- lib/formalist/elements.rb
+++ lib/formalist/elements.rb
@@ -4,6 +4,7 @@ require "formalist/elements/compound_field"
 require "formalist/elements/field"
 require "formalist/elements/group"
 require "formalist/elements/many"
+require "formalist/elements/many_forms"
 require "formalist/elements/section"
 
 module Formalist
@@ -15,6 +16,7 @@ module Formalist
     register :field, Field
     register :group, Group
     register :many, Many
+    register :many_forms, ManyForms
     register :section, Section
   end
 end
