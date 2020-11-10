98bf3457f77c718aa739b241addae13790446e40
narinda <narindajane@gmail.com>
2020-11-11 10:43:07 +1100

    Add validity check for a list of child forms

---
+++ lib/formalist/child_forms/validity_check.rb
@@ -0,0 +1,33 @@
+require "json"
+require_relative "builder"
+
+module Formalist
+  module ChildForms
+    class ValidityCheck
+      attr_reader :embedded_forms
+
+      def initialize(embedded_form_collection)
+        @embedded_forms = embedded_form_collection
+      end
+
+      def call(input)
+        return input if input.nil?
+
+        input = input.is_a?(String) ? JSON.parse(input) : input
+
+        input.map { |node| valid?(node) }.all?
+      end
+      alias_method :[], :call
+
+      private
+
+      def valid?(node)
+        name, data = node.values_at(:name, :data)
+
+        validation = embedded_forms[name].schema
+        validation.(data).success?
+      end
+
+    end
+  end
+end
