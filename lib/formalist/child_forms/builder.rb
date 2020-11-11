c51d598bd1a4a6c7bcf95ccacae10bc9944968d8
narinda <narindajane@gmail.com>
2020-11-12 09:29:22 +1100

    Fix bug in child forms builder
    The name in the many forms data is stored in the JSON as a string. The child form element expects the hash key to be a symbol for the element name.

--- lib/formalist/child_forms/builder.rb
+++ lib/formalist/child_forms/builder.rb
@@ -25,7 +25,7 @@ module Formalist
         name, data = node.values_at(:name, :data)
 
         embedded_form = embedded_forms[name]
-        child_form(name, embedded_form).fill(input: {name => data})
+        child_form(name, embedded_form).fill(input: {name.to_sym => data})
       end
 
       def child_form(name, embedded_form)
